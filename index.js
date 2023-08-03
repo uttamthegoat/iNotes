dfce07a389427f570122cddf818fee6e4c5ff881
uttamthegoat <118157434+uttamthegoat@users.noreply.github.com>
2023-08-03 22:41:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 connectDB();
 
 // middleware
-app.use(cors({origin:"*"}));
+app.use(cors());
 app.use(express.json());
 // Available Routes
 app.use("/api/auth", require("./routes/auth.js"));
